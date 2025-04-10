4bbb40ba4d2debc979caf24253d4c416740ce474
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-10 22:07:41 +0500

    adds required ticket pojo fields

--- src/main/java/net/hamidz/airTicketReservation/controller/publicController.java
+++ src/main/java/net/hamidz/airTicketReservation/controller/publicController.java
@@ -10,16 +10,4 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/public")
 public class publicController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/health-check")
-    public String check() {
-        return "Hello World!, 100% charged :)";
-    }
-
-    @PostMapping("/register-user")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
-        return new ResponseEntity<>(userService.saveNewUser(user), HttpStatus.CREATED);
-    }
 }
--- src/main/java/net/hamidz/airTicketReservation/entity/Ticket.java
+++ src/main/java/net/hamidz/airTicketReservation/entity/Ticket.java
@@ -1,5 +1,61 @@
 package net.hamidz.airTicketReservation.entity;
 
-public class Ticket {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
+
+@Document(collection = "tickets")
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+public class Ticket {
+    @Id
+    private ObjectId id;
+    @NonNull
+    @JsonProperty("ticketType")
+    private String ticketType;
+    @NonNull
+    @JsonProperty("ticketNumber")
+    private Long ticketNumber;
+    @NonNull
+    @JsonProperty("bookingDate")
+    private LocalDateTime bookingDate;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public @NonNull String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(@NonNull String ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public @NonNull Long getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(@NonNull Long ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public @NonNull LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(@NonNull LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
 }
