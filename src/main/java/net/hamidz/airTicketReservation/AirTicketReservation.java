8fea179c55273a52b2f778422fb06e0db0fc4d34
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-10 21:45:11 +0500

    added dependency and properties files for database connection

---
+++ src/main/java/net/hamidz/airTicketReservation/AirTicketReservation.java
@@ -0,0 +1,13 @@
+package net.hamidz.airTicketReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTicketReservation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTicketReservation.class, args);
+	}
+
+}
---
+++ src/test/java/net/hamidz/airTicketReservation/AirTicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamidz.airTicketReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
